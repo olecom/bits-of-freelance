11b5688af63ff562357eb7f75466b23695b2887b
Oleg Verych <olecom@gmail.com>
2014-11-01 03:32:00 +0400

    initial boilerplate: Collect and Map Accuray Places
    developed in Google Chrome

---
+++ accuray.com/collect_places_make_map.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            Collect Accuray Places
+// @author          olecom
+// @namespace       accuray
+// @description     Collect Accuray Places
+// @match           http://accuray.com/treatment-centers*
+// @version         0.1
+// ==/UserScript==
+
+function uglify_js(w ,doc ,con ,store){
+    return
+
+function mkClick(){
+    var ev = ce("MouseEvents")
+    ev.initMouseEvent("click", true, true, w, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    return ev
+}
+function gi(i){ return doc.getElementById(i) }
+function qs(s){ return doc.querySelector(s) }
+function qa(s){ return doc.querySelectorAll(s) }
+//function gs(n){ return doc.getElementsByTagName(n) }
+function ce(v){ return doc.createEvent(v) }
+function cl(t){ return doc.createElement(t) }
+}
+
+uglify_js(window, document, console, localStorage)
